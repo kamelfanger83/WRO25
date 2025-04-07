ae6607cad1c05414c905aab719ed61e903942a41
kamelfanger83 <linus83luechinger@gmail.com>
2025-04-07 12:14:25 +0200

    Debug

--- rpi/camera/find_line.cpp
+++ rpi/camera/find_line.cpp
@@ -13,22 +13,31 @@
 /// Assumptions: There is only one line.
 /// Assumptions: The points outside the field are ment to be already filtered
 /// out.
+// TODO: make this return an std::optional
 ScreenLine findLine(std::vector<Point> points) {
   if (points.size() < 10) {
     std::cerr << "Unfortunately there were to few points" << std::endl;
+    // TODO: return None
     assert(false);
   }
 
-  int distanceThreshold = 10;
+  struct ProbedLine {
+    Point *p1, *p2;
+    double angle;
+  };
 
-  std::vector<ScreenLine> lines;
+  std::vector<ProbedLine> lines;
 
-  for (int i = 0; i < int(points.size() / 2); ++i) {
-    int index1 = rand() % int(points.size());
-    int index2 = rand() % int(points.size());
+  const int distanceThreshold = 100;
+  const int targetLineCount = 1000;
 
-    int Δx = points[index1].x - points[index2].x;
-    int Δy = points[index1].y - points[index2].y;
+  for (int tries = 0; lines.size() < targetLineCount && tries < 10000;
+       ++tries) {
+    Point &p1 = points[rand() % int(points.size())];
+    Point &p2 = points[rand() % int(points.size())];
+
+    int Δx = p1.x - p2.x;
+    int Δy = p1.y - p2.y;
     int distance_sq = Δx * Δx + Δy * Δy;
 
     if (distance_sq <= distanceThreshold)
@@ -38,70 +47,96 @@ ScreenLine findLine(std::vector<Point> points) {
     if (angle < 0)
       angle += M_PI;
     assert(angle >= 0 && angle <= M_PI); // Ensure angle is in range [0, π]
-    double distanceToOrigin =
-        std::abs(-points[index1].x * Δy + points[index1].y * Δx) /
-        std::sqrt(distance_sq); // Calculate distance to origin
 
-    lines.push_back(
-        ScreenLine{angle, distanceToOrigin}); // Store line parameters
+    lines.push_back({&p1, &p2, angle}); // Store line parameters
+  }
+
+  if (lines.size() < targetLineCount) {
+    std::cerr << "The points were too close together, insufficient lines could "
+                 "be formed"
+              << std::endl;
+    // TODO: return None
+    assert(false);
   }
 
   std::sort(lines.begin(), lines.end(),
-            [](const ScreenLine &a, const ScreenLine &b) {
+            [](const ProbedLine &a, const ProbedLine &b) {
               return a.angle < b.angle; // Sort lines by angle
             });
 
   int countMax = -1;
-  int indexMax = -1;
+  double maxRangeL, maxRangeR;
 
-  for (int i = 0; i != int(lines.size()); ++i) {
-    // double ref = lines[i].angle;
-    int count = 1;
-    while (count < int(lines.size()) &&
-           lines[(i + count) % lines.size()].angle +
-                   (i + count >= int(lines.size()) ? M_PI : 0) -
-                   lines[i].angle <
-               0.1)
-      ++count;
+  const float MAX_DIF = 0.02;
+
+  for (int i = 0, r = 1; i < int(lines.size()); ++i) {
+    if (r == i)
+      r = i + 1;
+    while ((r + 1) % lines.size() != unsigned(i) &&
+           lines[r].angle + (r < i ? M_PI : 0) - lines[i].angle < MAX_DIF) {
+      r = (r + 1) % lines.size();
+    }
+    int count = r - i;
+    if (r < i) {
+      count = r + lines.size() - i;
+    }
     if (count > countMax) {
       countMax = count;
-      indexMax = i;
+      maxRangeL = lines[i].angle;
+      maxRangeR = lines[(r - 1 + lines.size()) % lines.size()].angle;
     }
   }
 
-  double sum = 0;
+  double finalAngle = (maxRangeL + maxRangeR) / 2;
+
+  if (finalAngle >= M_PI)
+    finalAngle -= M_PI;
+
+  // Find averge distanceToOrigin of all lines which fall into the angle range.
+  double sum = 0, cnt = 0;
 
   for (auto seg : lines) {
-    sum += seg.distanceToOrigin;
+    // Ensure that both cases (normal angle range or angle range which goes
+    // above PI) are handled correctly.
+    if ((maxRangeL <= seg.angle && seg.angle <= maxRangeR) ||
+        (maxRangeL <= seg.angle + M_PI && seg.angle + M_PI <= maxRangeR)) {
+
+      auto getDistanceToOrigin = [finalAngle](Point &p) -> double {
+        return -std::sin(finalAngle) * p.x + std::cos(finalAngle) * p.y;
+      };
+      double distanceToOrigin =
+          (getDistanceToOrigin(*seg.p1) + getDistanceToOrigin(*seg.p2)) / 2.;
+      sum += distanceToOrigin;
+      cnt += 1.;
+    }
   }
-  sum /= lines.size();
 
-  std::cerr << "we made it through" << std::endl;
+  if (cnt < 50.) {
+    std::cerr << "Too few lines were in the best range" << std::endl;
+    // TODO: return None
+    assert(false);
+  }
 
-  return ScreenLine{lines[indexMax].angle, sum};
+  return ScreenLine{finalAngle, sum / cnt};
 }
 
 /// This function takes a frame and a line and draws the line in the frame.
 /// It colors the line in green.
 /// WARNING: This function modifies the frame that is passed.
 void colorLineInFrame(Frame &frame, const ScreenLine &Line) {
-  std::cerr << "AAAsupi" << std::endl;
-  int x1 = sin(Line.angle) * Line.distanceToOrigin;
+  int x1 = -sin(Line.angle) * Line.distanceToOrigin;
   int y1 = cos(Line.angle) * Line.distanceToOrigin;
-  std::cerr << "BBBsupi" << std::endl;
 
   double m = tan(Line.angle);
 
   for (int x = 0; x < WIDTH; ++x) {
     int y = m * (x - x1) + y1;
     if (y >= 0 && y < HEIGHT) {
-      std::cerr << "supi" << std::endl;
       HSVPixel &pixel = frame.HSV[y * WIDTH + x];
       pixel.h = 200;
       pixel.s = 255;
       pixel.v = 255;
-    } else
-      std::cerr << "rip" << std::endl;
+    }
   }
 
   saveFrame(frame);
