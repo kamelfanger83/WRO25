b9b07f585e3ea4de438bd50a439904e3394f5cf1
Leo Amstutz <lamstutz@student.ethz.ch>
2025-05-03 12:32:28 +0200

    fix stupid error by the chef, create obstacleRound.cpp

---
+++ rpi/obstacleRound.cpp
@@ -0,0 +1,40 @@
+#include <chrono>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <optional>
+#include <queue>
+#include <thread>
+
+#include "camera/camera.h"
+#include "camera/find_color.cpp"
+#include "camera/find_line.cpp"
+#include "geo/camera.cpp"
+#include "getCommands/getCommands.cpp"
+#include "serial.cpp"
+#include "structs.h"
+
+bool trafficlight(Pose position) {
+  /// assume vguerins function is here
+}
+
+struct Mode {
+  std::function<std::optional<std::pair<std::queue<Waypoint>, Mode>>(
+      const Frame &, const Pose &)>
+      plan;
+};
+
+std::optional<std::pair<std::queue<Waypoint>, Mode>>
+endMode(const Frame &frame, const Pose &position) {
+  return {};
+}
+
+std::optional<std::pair<std::queue<Waypoint>, Mode>>
+startMode(const Frame &frame, const Pose &position) {
+  std::queue<Waypoint> waypoints;
+  Waypoint startPoint;
+  startPoint.x = position.x;
+  startPoint.y = position.y;
+
+  waypoints.pushback
+}
